0fd4593a2f27a233c002c51219174d016ea505f2
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2014-06-19 15:46:20 -0300

    Submissão inicial.
    	new file:   .gitignore
    	new file:   Makefile
    	new file:   main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: phillipe
+ *
+ * Created on June 19, 2014, 3:43 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
+
